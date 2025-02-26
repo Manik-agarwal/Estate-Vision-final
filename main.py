51781b88bc9b21ab163c7252a1a42e78c302ae61
Manik Agarwal <144595252+Manik-agarwal@users.noreply.github.com>
2025-02-27 01:45:04 +0530

    commit

--- main.py
+++ main.py
@@ -6,7 +6,21 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+# @app.route('/')
+def about():
+    return render_template('about.html')
+
+@app.route('/ai_prediction.html')
+def contact():
+    return render_template('ai_prediction.html')
+
+@app.route('/gallery')
+def services():
+    return render_template('gallery.html')
+
 if __name__=="__main__":
     app.run(debug=True,port=5001)
+
+
 
 
