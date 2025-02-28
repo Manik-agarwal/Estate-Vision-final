0f434fff8df8b56ee36d33c6c233bd4d1334d46f
Manik Agarwal <144595252+Manik-agarwal@users.noreply.github.com>
2025-03-01 01:08:50 +0530

    commit

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
 from flask import Flask,render_template,request
-import pandas as pd 
+import pandas as pd
+import pickle
+import numpy as np
 
 
 app=Flask(__name__)
 data=pd.read_csv("ML model/cleaned.csv")
+pipe=pickle.load(open("ML model/RidgeModel.pkl",'rb'))
 
 @app.route('/')
 def index():
-
-    locations=sorted(data['location'].unique())
     
-    return render_template('index.html',locations=locations)
+    return render_template('index.html')
 
-# @app.route('/')
-def about():
-    return render_template('about.html')
 
-@app.route('/ai_prediction.html')
+
+@app.route('/ai_prediction')
 def contact():
-    return render_template('ai_prediction.html')
+    locations=sorted(data['location'].unique())
+    return render_template('ai_prediction.html',locations=locations)
 
 @app.route('/gallery')
 def services():
     return render_template('gallery.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    location=request.form.get('location')
+    bhk=request.form.get('bhk')
+    bath=request.form.get('bath')
+    sqft=request.form.get('total_sqft')
+
+    input=pd.DataFrame([[location,sqft,bath,bhk]],columns=['location','total_sqft','bath','bhk'])
+    prediction=pipe.predict(input)[0]*1e5
+    return str(np.round(prediction,2))
 
 if __name__=="__main__":
     app.run(debug=True,port=5001)
